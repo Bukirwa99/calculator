a993b5420529ac41be33771a1b0a8d943a058012
bukirwa99 <angellabukirwa99@gmail.com>
2020-08-05 16:37:11 +0300

    formation

---
+++ my.js
@@ -0,0 +1,14 @@
+function getHistory(){
+  return  document.getElementById("history-value").innerText;
+
+}
+
+function printHistory(num){
+    document.getElementById("histiry-value").innerText=num;
+}
+function getOutput(num){
+    return document.getElementById("output-value").innerText=num;
+}
+function printOutput(num){
+    document.getElementById("output-value").innerText=num;
+}
